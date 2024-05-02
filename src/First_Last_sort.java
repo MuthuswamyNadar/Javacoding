755c44171f15bc683c62bf1dbeae67bc7d02c77b
Swamy <muthuswamynadar25@gmail.com>
2024-05-03 00:04:35 +0530

    push

--- src/First_Last_sort.java
+++ src/First_Last_sort.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class First_Last_sort {
+public class First_Last_sort {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] searchRange(int [] nums,int target)
+    {
+        int [] ans={-1,-1};
+        ans[0]=search(nums,target,true);
+        ans[1]=search(nums,target,false);
+        return ans;
+    }
+
+    public int search(int[] nums,int target,boolean findstartindex)
+    {
+        int start=0;
+        int end=nums.length;
+
+        while (start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(target<nums[mid])
+            {
+                end=mid-1;
+            }
+            else if(target>nums[mid])
+            {
+                start=mid+1;
+            }
+        }
+        return -1;
+    }
+
 }
